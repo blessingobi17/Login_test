b03c934d46a947699c5cc3186dc8ba558d4052f2
blessingobi17 <bjoseph863@gmail.com>
2025-06-13 16:44:46 +0100

    backend change

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://logintestt.netlify.app'
+  }));
 app.use(express.json());
 
 app.post('/login', async (req, res) => {
